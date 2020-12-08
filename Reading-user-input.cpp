1bc6f722b190ea2263cda2890550542d9dea9e4f
leloteso <47319288+leloteso@users.noreply.github.com>
2020-12-08 10:28:57 -0800

    Update Reading-user-input.cpp

--- Reading-user-input.cpp
+++ Reading-user-input.cpp
@@ -2,8 +2,8 @@
 using namespace std; 
 int main() { 
 int num;
-cout << "please input your age."; // c++ statement.
+cout << "Please input your age."; // c++ statement.
 cin>>num; //reading value from the users.
-cout<<"your age is : "<<num; //displaying user input back to the screen.
+cout<<"Your age is : "<<num; //displaying user input back to the screen.
 return 0; 
 } //end of the main function/ program.
