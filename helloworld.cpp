f6dc5a0b929bdae3c0b95bf10e0f2358202ee3df
leloteso <47319288+leloteso@users.noreply.github.com>
2020-12-08 06:02:06 -0800

    Update helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,9 +2,9 @@
 /*
 The following c++ code is A fundamental componenet of every c++ programming
 */
-#include<iostream> 
+#include<iostream> //header
 using namespace std; 
 int main() { 
-cout << "Hello World!"; 
+cout << "Hello World!"; // c++ statement
 return 0; 
-} 
+} //end of the main function/ program
